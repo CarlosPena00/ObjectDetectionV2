0527321910234f08316b199426f34688c102811a
chcp <chcp@cin.ufpe.br>
2017-06-04 02:42:44 -0300

    Refactoring the old code

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -9,19 +9,20 @@ from tqdm import tqdm
 sys.path.insert(0, 'machine_learning')
 sys.path.insert(0, 'utils')
 from machine_learning import MachineLearning
+from utils import Utils
 
-## Constant
+# Constant
 
 HOG_URL = "./Descriptors/hog/"
 LBP_URL = "./Descriptors/lbp/"
 SAVE = "./Descriptors/Save/"
-SAMPLE_IMAGES ="./SampleImages/"
+SAMPLE_IMAGES = "./SampleImages/"
 RESULT = "./Results/"
 UTILS = "./utils/"
 HISTOGRAM = "Histogram/"
 NEGATIVE = "Negative/"
 POSITIVE = "Positive/"
-SCALER ="Scaler"
+SCALER = "Scaler"
 MODEL = "Model"
 LINEAR = "linear"
 RBF = "rbf"
@@ -36,60 +37,62 @@ if __name__ == "__main__":
         print "Error not flags: -c -h rbf rf linear || -l -h rbf rf linear"
     else:
         if sys.argv[1] == '-c':
-            if sys.argv[2] == '-h':              
+            Ut = Utils()
+            if sys.argv[2] == '-h':
+
                 print "------Getting Negative Samples from File------"
-                XN = mergeX(positive=0)
+                XN = Ut.mergeCSV(positive=False)
                 rowsN, colsN = XN.shape
                 YN = np.zeros(shape=(rowsN, 1), dtype=int)
                 print "------Getting Positive Samples from File------"
-                XP = mergeX(positive=1)
+                XP = Ut.mergeCSV(positive=True)
                 rowsP, colsP = XP.shape
                 YP = np.ones(shape=(rowsP, 1), dtype=int)
                 X = np.vstack((XP, XN))
                 y = np.vstack((YP, YN))
                 y = y.ravel()
-        
+
                 print "---------------Start The Model----------------"
-                ML = MachineLearning(X,y)
-                
+                ML = MachineLearning(X, y)
+
                 if sys.argv[3] == 'rbf':
-                    modelFile = SAVE+MODEL+RBF+SAV
-                    scalerFile = SAVE+SCALER+RBF+SAV
-                    classifier,cm, standardScaler = ML.svm(kernel='rbf')
-                    
+                    modelFile = SAVE + RBF + MODEL + SAV
+                    scalerFile = SAVE + RBF + SCALER + SAV
+                    classifier, cm, standardScaler = ML.svm(kernel='rbf')
+
                 if sys.argv[3] == 'rf':
-                    modelFile = SAVE+MODEL+RF+SAV
-                    scalerFile = SAVE+SCALER+RF+SAV
-                    classifier,cm, standardScaler = ML.randomForest(N=100,theads=3)
-                    
+                    modelFile = SAVE + RF + MODEL + SAV
+                    scalerFile = SAVE + RF + SCALER + SAV
+                    classifier, cm, standardScaler = ML.randomForest(
+                        N=100, theads=3)
+
                 if sys.argv[3] == 'linear':
-                    modelFile = SAVE+MODEL+LINEAR+SAV
-                    scalerFile = SAVE+SCALER+LINEAR+SAV
-                    X_train,X_test,y_train,y_test,y_pred,classifier,cm,standardScaler = ML.linearSvm(Ce=0.01)
-        
+                    modelFile = SAVE + LINEAR + MODEL + SAV
+                    scalerFile = SAVE + LINEAR + SCALER + SAV
+                    X_train, X_test, y_train, y_test, y_pred, classifier, cm, standardScaler = ML.linearSvm(
+                        Ce=0.01)
+
                 print "-----------------Save The Model---------------"
                 pickle.dump(classifier, open(modelFile, 'wb'))
                 pickle.dump(standardScaler, open(scalerFile, 'wb'))
-        
+
                 print "-----------------Train The Model--------------"
                 train(classifier, standardScaler, std=1)
-            
+
             if sys.argv[2] == 'l':
                 print "------NOT YET!------"
-    
+
         if sys.argv[1] == '-l':
-            if sys.argv[2] == '-h': 
+            if sys.argv[2] == '-h':
                 if sys.argv[3] == 'rbf':
-                    modelFile = SAVE+MODEL+RBF+SAV
-                    scalerFile = SAVE+SCALER+RBF+SAV
+                    modelFile = SAVE + RBF + MODEL + SAV
+                    scalerFile = SAVE + RBF + SCALER + SAV
                 if sys.argv[3] == 'rf':
-                    modelFile = SAVE+MODEL+RF+SAV
-                    scalerFile = SAVE+SCALER+RF+SAV
+                    modelFile = SAVE + RF + MODEL + SAV
+                    scalerFile = SAVE + RF + SCALER + SAV
                 if sys.argv[3] == 'linear':
-                    modelFile = SAVE+MODEL+LINEAR+SAV
-                    scalerFile = SAVE+SCALER+LINEAR+SAV
+                    modelFile = SAVE + LINEAR + MODEL + SAV
+                    scalerFile = SAVE + LINEAR + SCALER + SAV
                 classifier = pickle.load(open(modelFile, 'rb'))
                 standardScaler = pickle.load(open(scalerFile, 'rb'))
                 train(classifier, standardScaler, std=1)
-    
-        
--- utils/utils.py
+++ utils/utils.py
@@ -21,22 +21,21 @@ PNG = ".png"
 SAV = ".sav"
 CSV = ".csv"
 
-negativeList = "negativeFiles"
-positiveList = "positiveFiles"
+negativeList = "utils/negativeFiles"
+positiveList = "utils/positiveFiles"
 NUMBER_OF_DIMS = 1764
 
 class Utils:
     'Commoun function'
     
-    def mergeCSV(positive=True, descriptor="HOG"):
-        csvFiles =''
-        
+    def mergeCSV(self,positive=True, descriptor="HOG"):
         if descriptor == "HOG":
             csvFiles = HOG_URL
         if descriptor == "LBP":
             csvFiles = LBP_URL
         if positive == True:
             csvFiles += POSITIVE
+            print csvFiles
             csvList = positiveList + CSV
         else:
             csvFiles += NEGATIVE
@@ -44,8 +43,8 @@ class Utils:
         dataList = pd.read_csv(csvList).iloc[:,:].values
         xList = np.empty([1, NUMBER_OF_DIMS], dtype="float32")
         for folder in dataList:
-            print "----- Start To Get Folder " + csvFiles + folder + " -----"
-            dataset = pd.read_csv(csvFiles + folder[0], dtype="float32")
+            print "----- Start To Get Folder " + csvFiles + folder[0] + " -----"
+            dataset = pd.read_csv(csvFiles + folder[0] + CSV, dtype="float32")
             xNew = dataset.iloc[:, :].values
             xList = np.vstack((xList, xNew))
         xList = np.delete(xList, (0), axis=0)
@@ -56,5 +55,5 @@ class Utils:
 if __name__ == "__main__":
     ut = Utils()
     
-    ut.mergeCSV(True)
+   #ut.mergeCSV(True)
     
